aafc54e5e869763b67edb882b4b5cb240f767a81
Rohan <rohaninfocepts@gmail.com>
2017-07-21 13:24:08 +0530

    Added a fix for route handling in production.

---
+++ src/main/java/com/icms/internal/routehandler/RouteHandler.java
@@ -0,0 +1,17 @@
+package com.icms.internal.routehandler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Infocepts India in 2017.
+ */
+
+@Controller
+public class RouteHandler
+{
+    @RequestMapping ({ "/IcmsLogin", "/ICMS/**" })
+    public String index() {
+        return "forward:/index.html";
+    }
+}
